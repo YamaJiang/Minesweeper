02f5ac22037e8d883054b4941f9c9f2697f04c1c
Yama Jiang <yamajiang.cs@gmail.com>
2024-06-24 20:31:43 -0400

    Frame

--- Minesweeper/src/Minesweeper.java
+++ Minesweeper/src/Minesweeper.java
@@ -6,4 +6,21 @@ import javax.swing.*;
 
 public class Minesweeper{
 
+    int tileSize=70;
+    int numRows= 8;
+    int numCols= numRows;
+    int boardWidth= numCols * tileSize;
+    int boardHeight= numRows*tileSize;
+
+    JFrame frame = new JFrame("Minesweeper");
+
+    Minesweeper(){
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+    }
 }
